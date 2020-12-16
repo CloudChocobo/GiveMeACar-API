6c038b4bfa577571f11f92244d1c73fe6e367e93
Joseph Allain <36985411+Gazzia29@users.noreply.github.com>
2020-12-16 10:21:31 +0100

    sprint 4 oublis

---
+++ src/main/java/com/grouped/gmacrest/GmacrestApplication.java
@@ -0,0 +1,13 @@
+package com.grouped.gmacrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmacrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmacrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grouped/gmacrest/GmacrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grouped.gmacrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmacrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
